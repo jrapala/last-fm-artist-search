5ba24494ddc9f8a7f101472a72f1eb38ebf400a8
Juliette Rapala <juliette@eventric.com>
2020-07-03 17:36:18 -0500

    Finish LoginPage styles
    - Add Logo
    - Add last.fm Logo
    - Create Card component
    - Create Button component
    - Update Footer
    - Expand theme

--- src/components/organisms/Footer/index.tsx
+++ src/components/organisms/Footer/index.tsx
@@ -1,17 +1,33 @@
 import React from "react"
 import styled from "styled-components"
 
+import LastFMLogo from "../../atoms/LastFMLogo"
+
 const Footer: React.FC = () => {
 	return (
 		<Container>
-			<p>Powered by last.fm</p>
+			<p>Powered by</p>
+			<a href="https://www.last.fm/" target="_blank" rel="noreferrer">
+				<LastFMLogo width="100%" />
+			</a>
 		</Container>
 	)
 }
 
 const Container = styled.footer`
+	align-items: center;
 	display: flex;
 	justify-content: center;
+	padding-bottom: 0.5em;
+
+	p {
+		margin-right: 0.5em;
+	}
+
+	a {
+		display: flex;
+		width: 20%;
+	}
 `
 
 export default Footer
